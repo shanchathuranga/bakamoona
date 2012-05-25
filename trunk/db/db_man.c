5fef5e317f0523409ec58e24ccd8a8621a5a765c
shanchathuranga@gmail.com <shanchathuranga@gmail.com@7925504f-d703-0b41-c320-4ab83f0e13f4>
2012-05-25 05:21:28 +0000

    formatted

--- trunk/db/db_man.c
+++ trunk/db/db_man.c
@@ -4,20 +4,20 @@
 
 void bakamoona_wakeup ( const char * url, const char * username, const char * password)
 {
-	conn = mysql_init(NULL);
+    conn = mysql_init(NULL);
 
-	if (conn == NULL ){
-		printf("ERROR : bakamoona wakeup error\n");
-		exit(1);
-	}
+	  if (conn == NULL ){
+	      printf("ERROR : bakamoona wakeup error\n");
+	      exit(1);
+	  }
 
-	if (mysql_real_connect(conn, url, username, password, "bakamoona", 0, NULL, 0) == NULL) {
-		printf("ERROR : bakamoona wakeup error");
-		exit(1);
-	}
+	  if (mysql_real_connect(conn, url, username, password, "bakamoona", 0, NULL, 0) == NULL) {
+	      printf("ERROR : bakamoona wakeup error");
+	      exit(1);
+	  }
 }
 
 void bakamoona_sleep ()
 {
-	mysql_close(conn);
+	  mysql_close(conn);
 }
