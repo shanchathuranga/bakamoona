4a0254a42e2dd385a1eeeaadd317a67d8adc4209
shanchathuranga <shanchathuranga@7925504f-d703-0b41-c320-4ab83f0e13f4>
2012-05-26 06:03:38 +0000

    Adding Makefile, main.c

--- trunk/db/db_man.c
+++ trunk/db/db_man.c
@@ -12,7 +12,30 @@ void bakamoona_wakeup ( const char * url, const char * username, const char * pa
 	}
 
 	if (mysql_real_connect(conn, url, username, password, "bakamoona", 0, NULL, 0) == NULL) {
-		printf("ERROR : bakamoona wakeup error");
+		printf("ERROR : bakamoona wakeup error\n");
+		exit(1);
+	}
+}
+
+void bakamoona_init_db ()
+{
+	if (mysql_query(conn, BKM_CREATE_QUERY_JOBDETAILS)) {
+		printf("ERROR : bakamoona create BKM_CREATE_QUERY_JOBDETAILS error\n");
+		exit(1);
+	}
+	
+	if (mysql_query(conn, BKM_CREATE_QUERY_BUS)) {
+		printf("ERROR : bakamoona create BKM_CREATE_QUERY_BUS error\n");
+		exit(1);
+	}
+
+	if (mysql_query(conn, BKM_CREATE_QUERY_OWNER)) {
+		printf("ERROR : bakamoona create BKM_CREATE_QUERY_OWNER error\n");
+		exit(1);
+	}
+
+	if (mysql_query(conn, BKM_CREATE_QUERY_UNREGISTEREDJOBDETAILS)) {
+		printf("ERROR : bakamoona create BKM_CREATE_QUERY_OWNER error\n");
 		exit(1);
 	}
 }
--- trunk/db/db_man.h
+++ trunk/db/db_man.h
@@ -8,9 +8,50 @@
 #define SUCCESS          101
 #define MULTIPLE_RESULT  102
 
+#define BKM_CREATE_QUERY_JOBDETAILS "CREATE TABLE IF NOT EXISTS JobDetails ( \
+	jobdetail_id INT NOT NULL AUTO_INCREMENT, \
+	bus_id INT NOT NULL, \
+	spare_part_id INT, \
+	job_description VARCHAR (255), \
+	job_fee FLOAT (10,2), \
+	ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, \
+	PRIMARY KEY (jobdetail_id) \
+);"
+
+#define BKM_CREATE_QUERY_BUS "CREATE TABLE IF NOT EXISTS Bus ( \
+	bus_id INT NOT NULL AUTO_INCREMENT, \
+	owner_id INT NOT NULL, \
+	bus_reg_no VARCHAR (8), \
+	bus_model VARCHAR (64), \
+	ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, \
+	PRIMARY KEY (bus_id) \
+);"
+
+#define BKM_CREATE_QUERY_OWNER "CREATE TABLE IF NOT EXISTS Owner ( \
+	owner_id INT NOT NULL AUTO_INCREMENT, \
+	owner_name VARCHAR (128), \
+	company_name VARCHAR (64), \
+	ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, \
+	PRIMARY KEY (owner_id) \
+);"
+
+#define BKM_CREATE_QUERY_UNREGISTEREDJOBDETAILS "CREATE TABLE UnregistredJobDetails ( \
+	unreg_jobdetail_id INT NOT NULL AUTO_INCREMENT, \
+	bus_reg_no VARCHAR (8), \
+	bus_model VARCHAR (64), \
+	owner_name VARCHAR (128), \
+	company_name VARCHAR (64), \
+	spare_part_id INT, \
+	job_description VARCHAR (255), \
+	job_fee FLOAT (10,2), \
+	ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, \
+	PRIMARY KEY (unreg_jobdetail_id) \
+);"
+
 static MYSQL * conn = NULL;
 
 void bakamoona_wakeup ( const char * url, const char * username, const char * password);
+void bakamoona_init_db ();
 void bakamoona_sleep ();
 
 #endif
