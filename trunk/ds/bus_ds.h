c7c01397930573734642a3b443ae90fc612c5b71
shanchathuranga <shanchathuranga@7925504f-d703-0b41-c320-4ab83f0e13f4>
2012-05-27 12:20:37 +0000

    bus_ds.c Added

--- trunk/ds/bus_ds.h
+++ trunk/ds/bus_ds.h
@@ -10,9 +10,18 @@ typedef struct bus
 	int error;
 } BUS;
 
-typedef struct buslist
+typedef struct bus_list
 {
-
+	BUS * bus;
+	struct bus_list * next;
 } BUSLIST;
+
+typedef BUSLIST BUSNODE;
+
+BUSLIST * make_bus_list();
+void add_bus_to_list (BUSLIST * list, BUS * bus);
+BUSLIST * delete_bus_list (BUSLIST * list);
+static void recursive_delete (BUSLIST * list);
+void debug_bus_list (BUSLIST * list);
 
 #endif
