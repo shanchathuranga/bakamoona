44ea8f91a342a1cf4906946a6aa3b27043de4ac0
dilan404 <dilan404@7925504f-d703-0b41-c320-4ab83f0e13f4>
2012-05-31 05:13:29 +0000

    db file deleted

--- trunk/main.c
+++ trunk/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <string.h>
 struct Vehicle 
 {
 	int engineCapacity;
@@ -9,5 +9,14 @@ struct Vehicle
 
 int main()
 {
+
+  struct Vehicle v;
+
+      v.engineCapacity = 3000;
+      v.numGears = 5;
+      strcpy (v.fuelType, "DEISEL");
+printf ("Engine Capacity = %d\n", v.engineCapacity);
+printf ("No of Gers = %d\n", v.numGears);
+printf ("Fuel Type = %s\n", v.fuelType);
 	return 0;
 }
