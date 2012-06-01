425c6f9be69f558bc2a3091c90bf259333f1d17f
shanchathuranga <shanchathuranga@gmail.com>
2012-06-01 09:34:33 +0000

    main.c made empty

--- main.c
+++ main.c
@@ -1,22 +1,6 @@
 #include <stdio.h>
-#include <string.h>
-struct Vehicle 
-{
-	int engineCapacity;
-	int numGears;
-	char fuelType[20];
-};
 
 int main()
 {
-
-  struct Vehicle v;
-
-      v.engineCapacity = 3000;
-      v.numGears = 5;
-      strcpy (v.fuelType, "DEISEL");
-printf ("Engine Capacity = %d\n", v.engineCapacity);
-printf ("No of Gers = %d\n", v.numGears);
-printf ("Fuel Type = %s\n", v.fuelType);
 	return 0;
 }
