71bb7627570ab8eab28e696305733ae04632f781
shanchathuranga <shanchathuranga@7925504f-d703-0b41-c320-4ab83f0e13f4>
2012-06-03 16:29:49 +0000

    bus related files updated

--- db/bus.h
+++ db/bus.h
@@ -3,9 +3,23 @@
 
 #include "bus_ds.h"
 
+// select
 BUS * get_bus_by_id (int bus_id);
 BUS * get_bus_by_reg_no (char * bus_reg_no);
 BUSLIST * get_bus_by_owner_id (int owner_id);
 BUSLIST * get_bus_by_model (char * bus_model);
+
+// insert
+void insert_bus (BUS * bus);
+
+// update
+void update_bus (BUS * bus);
+void change_bus_reg_no (int bus_id, char * bus_reg_no);
+void change_bus_owner_id (int bus_id, int owner_id);
+void change_bus_model (int bus_id, char * bus_model);
+
+// delete
+void delete_bus (BUS * bus);
+void delete_bus_by_id (int bus_id);
 
 #endif
--- main.c
+++ main.c
@@ -1,22 +1,38 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "bus.h"
 
 int main()
 {
 	printf ("starting ........ \n");
-	BUS * bus = get_bus_by_id (3);	
+	BUS * bus = get_bus_by_id (3);
 
 	printf ("bus_id = %d\n", bus->bus_id);
 	printf ("owner_id = %d\n", bus->owner_id);
 	printf ("bus_reg_no = %s\n", bus->bus_reg_no);
 	printf ("bus_model = %s\n", bus->bus_model);
-	printf ("ts ??? \n");	
+	printf ("ts ??? \n");
 
-	delete_bus (bus);
+	remove_bus (bus);
 
 	BUSLIST * list = get_bus_by_owner_id (23201);
 	debug_bus_list (list);
+
+	BUS * b1 = malloc (sizeof(BUS));
+	b1->owner_id = 38332;
+	strcpy (b1->bus_reg_no, "62-5933");
+	strcpy (b1->bus_model, "MITSUBISHI");
+
+	//insert_bus (b1);
+	BUS * b2 = get_bus_by_id (3);
+	strcpy (b2->bus_model, "micro-SS9");
+
+	update_bus (b2); 
+
+	remove_bus (b1);
+
+	delete_bus_by_id (16);
 
 	return 0;
 }
