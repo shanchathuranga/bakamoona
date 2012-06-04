8381a7b7bf2accacd5773092ddbfceb314f3688e
shanchathuranga <shanchathuranga@7925504f-d703-0b41-c320-4ab83f0e13f4>
2012-06-04 12:58:08 +0000

    updated file menu

--- trunk/gui/mainwin.h
+++ trunk/gui/mainwin.h
@@ -6,8 +6,13 @@
 GtkWidget * mainwin;
 GtkWidget * layout;
 GtkWidget * menubar;
-GtkWidget * filemenu;
-GtkWidget * file;
+GtkWidget * addmenu;
+
+// Add menu
+GtkWidget * add;
+GtkWidget * add_bus;
+GtkWidget * add_owner;
+GtkWidget * add_sparepart;
 GtkWidget * quit;
 
 #endif
