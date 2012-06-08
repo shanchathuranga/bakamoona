88c3d7295d5148d1bdc8091bb8edb843d9c2eed4
shanchathuranga@gmail.com <shanchathuranga@gmail.com@7925504f-d703-0b41-c320-4ab83f0e13f4>
2012-06-08 09:31:05 +0000

    Adding owner_reg.h

---
+++ trunk/gui/owner_reg.h
@@ -0,0 +1,24 @@
+#ifndef _OWNER_REG_H_
+#define _OWNER_REG_H_
+
+#include <gtk/gtk.h>
+
+GtkWidget * window;
+GtkWidget * title;
+GtkWidget * vbox;
+GtkWidget * hbox;
+GtkWidget * hspace;
+GtkWidget * table;
+GtkWidget * sep;
+GtkWidget * ok;
+GtkWidget * cancel;
+GtkWidget * lname, * tname;
+GtkWidget * lregno, * tregno;
+GtkWidget * lph1, * tph1;
+GtkWidget * lph2, * tph2;
+GtkWidget * laddress1, * taddress1;
+GtkWidget * lcity1, * tcity1;
+GtkWidget * laddress2, * taddress2;
+GtkWidget * lcity2, * tcity2; 
+
+#endif
