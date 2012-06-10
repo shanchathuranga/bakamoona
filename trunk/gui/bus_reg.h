087471b99495cf69ee4f4dcdbda6018a57d76766
shanchathuranga <shanchathuranga@7925504f-d703-0b41-c320-4ab83f0e13f4>
2012-06-10 06:00:11 +0000

    bus registration form updated

--- trunk/gui/bus_reg.h
+++ trunk/gui/bus_reg.h
@@ -3,6 +3,40 @@
 
 #include <gtk/gtk.h>
 
-void create_bus_reg_window ();
+typedef struct _bus_ui
+{
+	GtkWidget * window;
+	GtkWidget * vbox;
+	GtkWidget * owner_table;
+	GtkWidget * bus_table;
+	GtkWidget * hbox;
+	GtkWidget * select_owner;
+	GtkWidget * clear;
+	GtkWidget * ok;
+	GtkWidget * cancel;
+	GtkWidget * title;
+	GtkWidget * owner_frame;
+	GtkWidget * bus_frame;
+	GtkWidget * lowner_name;
+	GtkWidget * lowner_address;
+	GtkWidget * lowner_city;
+	GtkWidget * towner_name;
+	GtkWidget * towner_address;
+	GtkWidget * towner_city;
+	GtkWidget * lbus_regno;
+	GtkWidget * lengine_no;
+	GtkWidget * lchassy_no;
+	GtkWidget * lbus_model;
+	GtkWidget * lbus_desc;
+	GtkWidget * tbus_regno;
+	GtkWidget * tengine_no;
+	GtkWidget * tchassy_no;	
+	GtkWidget * tbus_model;
+	GtkWidget * tbus_desc;
+} bus_ui;
+
+bus_ui * bui;
+
+bus_ui * create_bus_reg_window ();
 
 #endif
