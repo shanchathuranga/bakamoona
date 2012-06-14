700bc5432d74f29cbc92809e7675054288f73baa
shanchathuranga@gmail.com <shanchathuranga@gmail.com@7925504f-d703-0b41-c320-4ab83f0e13f4>
2012-06-14 09:22:15 +0000

    adding recursive_owner_delete () function

--- trunk/ds/owner_ds.h
+++ trunk/ds/owner_ds.h
@@ -27,7 +27,7 @@ typedef OWNERLIST OWNERNODE;
 OWNERLIST * make_owner_list ();
 void add_owner_to_list (OWNERLIST * list, OWNER * owner);
 OWNERLIST * delete_owner_list (OWNERLIST * list);
-//static void recursive_delete (OWNERLIST * list);
+static void recursive_owner_delete (OWNERLIST * list);
 void delete_owner (OWNER * bus);
 void debug_owner_list (OWNERLIST * list);
 
