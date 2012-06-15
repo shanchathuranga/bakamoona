570a9fe0c4ea3b2a89248b118e81bc94db196c5c
shanchathuranga@gmail.com <shanchathuranga@gmail.com@7925504f-d703-0b41-c320-4ab83f0e13f4>
2012-06-15 07:03:37 +0000

    owner_search_ui.c updated

--- trunk/gui/owner_search_ui.c
+++ trunk/gui/owner_search_ui.c
@@ -1,27 +1,4 @@
-#include <gtk/gtk.h>
-
-GtkWidget * window;
-GtkWidget * vbox;
-GtkWidget * title;
-GtkWidget * sw;
-GtkWidget * hbox;
-GtkWidget * refresh;
-GtkWidget * select;
-GtkWidget * cancel;
-
-enum
-{
-    COL_NAME = 0,
-    COL_REG_NO,
-    COL_PHONE_1,
-    COL_PHONE_2,
-    COL_ADDRESS_1, 
-    COL_CITY_1,
-    COL_ADDRESS_2,
-    COL_CITY_2,
-    COL_EMAIL,
-    COL_COUNT
-};
+#include "owner_search_ui.h"
 
 void add_data (GtkWidget * list, char * name, char * age)
 {
